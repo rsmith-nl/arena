5be220fe7584d090a7713942743144da5b98f898
Roland Smith <rsmith@xs4all.nl>
2023-05-02 00:28:52 +0200

    Added arena_remaining.

--- arena.c
+++ arena.c
@@ -4,10 +4,9 @@
  *  Copyright © 2023 R.F. Smith <rsmith@xs4all.nl>
  *  SPDX-License-Identifier: MIT
  *  Created: 2023-04-23T22:08:02 +0200
- *  Last modified: 2023-04-23T22:55:01+0200
+ *  Last modified: 2023-05-02T00:21:17+0200
  */
 
-#include <stdint.h>
 #include <stdio.h> /* for fprintf */
 #include <stdlib.h> /* for abort*/
 #include <string.h> /* for memset */
@@ -24,6 +23,7 @@ void arena_create(arena_t *arena, size_t length) {
     }
     arena->length = length;
     arena->used = 0;
+    arena->storage = 0;
     void *allocated = mmap(0, length, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
     if (allocated == MAP_FAILED) {
         fprintf(stderr, "memort for arena cannot be allocated");
@@ -32,7 +32,7 @@ void arena_create(arena_t *arena, size_t length) {
     arena->storage = (uint8_t *)allocated;
 }
 
-void *arena_allocate(arena_t *arena, size_t size) {
+size_t arena_remaining(arena_t *arena) {
     if (arena == 0) {
         fprintf(stderr, "invalid arena");
         abort();
@@ -42,6 +42,11 @@ void *arena_allocate(arena_t *arena, size_t size) {
         abort();
     }
     size_t remaining = arena->length - arena->used;
+    return remaining;
+}
+
+void *arena_allocate(arena_t *arena, size_t size) {
+    size_t remaining = arena_remaining(arena);
     if (size > remaining) {
         fprintf(stderr, "unable to allocate requested size");
         abort();
@@ -63,6 +68,5 @@ void arena_destroy(arena_t *arena) {
         abort();
     }
 }
-
 
 /* EOF */
--- arena.h
+++ arena.h
@@ -4,10 +4,12 @@
  *  Copyright © 2023 R.F. Smith <rsmith@xs4all.nl>
  *  SPDX-License-Identifier: MIT
  *  Created: 2023-04-23T22:07:59+0200
- *  Last modified: 2023-04-23T22:54:19+0200
+ *  Last modified: 2023-05-02T00:19:43+0200
  */
 
 #pragma once
+
+#include <sys/types.h> /* for uint8_t and size_t */
 
 typedef struct {
     size_t length;
@@ -20,6 +22,7 @@ extern "C" {
 #endif
 
 void arena_create(arena_t *arena, size_t length);
+size_t arena_remaining(arena_t *arena);
 void *arena_allocate(arena_t *arena, size_t size);
 void arena_destroy(arena_t *arena);
 
