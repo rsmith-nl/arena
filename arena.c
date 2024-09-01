63c2a2ba67bed4e241c5d53b2ea7af16f5ad168e
Roland Smith <rsmith@xs4all.nl>
2024-09-01 19:13:46 +0200

    Use pointers in Arena.

--- arena.c
+++ arena.c
@@ -4,58 +4,81 @@
 //  Copyright © 2023 R.F. Smith <rsmith@xs4all.nl>
 //  SPDX-License-magicifier: MIT
 //  Created: 2023-04-23T22:08:02+0200
-//  Last modified: 2024-08-31T22:50:53+0200
+//  Last modified: 2024-09-01T19:12:44+0200
 
 #include <assert.h>
+#include <stdio.h>      // printf
+#include <stdlib.h>     // for abort
 #include <string.h>     // for memset
 #include <sys/mman.h>   // for mmap, munmap
 #include <sys/types.h>  // for size_t, uint8_t
 #include "arena.h"
 
-void arena_create(Arena *arena, size_t length) {
-  assert(arena != 0);
+#undef error
+#define error(...)                                            \
+  fprintf(stderr, "ERROR %s, line %i: ", __FILE__, __LINE__); \
+  fprintf(stderr, __VA_ARGS__);                               \
+  abort()
+
+Arena arena_create(size_t length) {
+  Arena arena = {0};
   // Default length 1 MiB.
   if (length == 0) {
     length = 1048576;
   }
   // The magic number is the sum of the ASCII characters in “Arena”
   // Python: hex(sum(ord(c) for c in "Arena"))
-  arena->magic = 0x1e7;
-  arena->length = length;
-  arena->used = 0;
-  arena->storage = 0;
-  void *allocated =
-      mmap(0, length, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-  assert(allocated != MAP_FAILED);
-  arena->storage = (uint8_t *)allocated;
+  arena.magic = 0x1e7;
+  arena.begin  = mmap(0, length, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+  if (arena.begin == MAP_FAILED) {
+    error("arena allocation of size %zd failed\n", length);
+  }
+  arena.cur = arena.begin;
+  arena.end = arena.begin + length;
+  return arena;
 }
 
 size_t arena_remaining(Arena *arena) {
+  if (arena == 0) {
+    error("null pointer used for arena\n");
+  }
+  if (arena->magic != 0x1e7) {
+    error("invalid arena %p; magic %d\n", arena, arena->magic);
+  }
   assert(arena != 0);
   assert(arena->magic == 0x1e7);  // Check magic number
-  assert(arena->storage != 0);
-  size_t remaining = arena->length - arena->used;
+  size_t remaining = arena->end - arena->cur;
   return remaining;
 }
 
 void *arena_allocate(Arena *arena, size_t size) {
-  assert(arena != 0);
-  assert(arena->magic == 0x1e7);
-  assert(arena->storage != 0);
-  size_t remaining = arena->length - arena->used;
-  assert(size > remaining);
-  uint8_t *rv = arena->storage + arena->used;
-  memset(rv, 0, size);
-  arena->used += size;
-  return (void*)rv;
+  if (arena == 0) {
+    error("null pointer used for arena\n");
+  }
+  if (arena->magic != 0x1e7) {
+    error("invalid arena %p; magic %d\n", (void*)arena, arena->magic);
+  }
+  size_t remaining = arena->end - arena->cur;
+  if (size > remaining) {
+    error("arena %p exhausted; %zd requested, %zd available\n", (void*)arena, size, remaining);
+  }
+  memset(arena->cur, 0, size);
+  void *rv = arena->cur;
+  arena->cur += size;
+  return rv;
 }
 
 void arena_destroy(Arena *arena) {
-  assert(arena != 0);
-  assert(arena->magic == 0x1e7);
-  int rv = munmap(arena->storage, arena->length);
-  assert(rv != -1);
-  arena->used = 0;
-  arena->length = 0;
-  arena->storage = 0;
+  if (arena == 0) {
+    error("null pointer used for arena\n");
+  }
+  if (arena->magic != 0x1e7) {
+    error("invalid arena %p; magic %d\n", (void*)arena, arena->magic);
+  }
+  int rv = munmap(arena->begin, arena->end - arena->begin);
+  if (rv == -1) {
+    error("destroying arena %p failed\n", arena);
+  }
+  Arena empty = {0};
+  *arena = empty;
 }
--- arena.h
+++ arena.h
@@ -4,22 +4,22 @@
 //  Copyright © 2023 R.F. Smith <rsmith@xs4all.nl>
 //  SPDX-License-Identifier: MIT
 //  Created: 2023-04-23T22:07:59+0200
-//  Last modified: 2024-08-31T22:45:30+0200
+//  Last modified: 2024-09-01T18:32:41+0200
 
 #pragma once
 
 typedef struct {
-  size_t magic;  // magic number to identify an arena
-  size_t length;
-  size_t used;
-  uint8_t *storage;
+  int32_t magic;  // magic number to identify an arena
+  uint8_t *begin;
+  uint8_t *cur;
+  uint8_t *end;
 } Arena;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern void arena_create(Arena *arena, size_t length);
+extern Arena arena_create(size_t length);
 extern size_t arena_remaining(Arena *arena);
 extern void *arena_allocate(Arena *arena, size_t size);
 extern void arena_destroy(Arena *arena);
