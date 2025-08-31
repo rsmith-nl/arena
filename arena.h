d2dc35be99c9ea0469b6a25ea1445ffd64edb626
Roland Smith <rsmith@xs4all.nl>
2025-08-31 09:56:12 +0200

    Improve comments.

--- arena.h
+++ arena.h
@@ -5,7 +5,9 @@
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2023-04-23T22:07:59+0200
-// Last modified: 2025-08-28T22:12:04+0200
+// Last modified: 2025-08-31T09:55:38+0200
+
+// Inspired by: https://nullprogram.com/blog/2023/09/27/
 
 #pragma once
 
@@ -24,14 +26,14 @@ typedef struct {
 extern "C" {
 #endif
 
-// Create an arena of the given *length*. If *length* is 0, 1 MiB is used.
+// Create an arena of the given “length”. If “length” is 0, 1 MiB is used.
 extern Arena arena_create(ptrdiff_t length);
-// Return the remaining space in the arena.
+// Return the remaining space in bytes in the arena.
 extern ptrdiff_t arena_remaining(Arena *arena);
-// Allocate from the arena. Use arena_new instead.
+// Allocate from the arena. Best to use arena_new instead.
 extern void *arena_alloc(Arena *arena, ptrdiff_t size, ptrdiff_t count,
                          ptrdiff_t align);
-// Allocate *n* objects of *type* from arena *a*.
+// Allocate “n” objects of type “t” from arena “a”.
 #define arena_new(a, t, n) (t *)arena_alloc(a, sizeof(t), n, alignof(t))
 // Destroy the arena and make it invalid.
 extern void arena_destroy(Arena *arena);
