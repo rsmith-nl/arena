40df1e3e989d219324984267916ffc4aa68214b2
Roland Smith <rsmith@xs4all.nl>
2025-09-08 21:25:36 +0200

    Write arena test program.

--- Makefile
+++ Makefile
@@ -1,13 +1,14 @@
 CFLAGS = -pipe -std=c11 -fPIC -g3 -Wall -Wextra -Wstrict-prototypes -Wpedantic \
                 -Wshadow-all -Wmissing-field-initializers -Wpointer-arith
 
-check: arena.o
+check:
+	$(CC) $(CFLAGS) -c arena.c
 	clang-tidy19 --quiet *.c *.h 2>/dev/null
 	rm -f *.o
 
-.c.o:
-	$(CC) $(CFLAGS) $(CPPFLAGS) $(HDIRS) -c -o $@ $<
+test:
+	$(CC) $(CFLAGS) -o arena_test arena_test.c arena.c
 
 .PHONY: clean
 clean:
-	rm -f *.o
+	rm -f *.o arena_test
---
+++ arena_test.c
@@ -0,0 +1,50 @@
+// file: arena_test.c
+// vim:fileencoding=utf-8:ft=c:tabstop=2
+// This is free and unencumbered software released into the public domain.
+//
+// Author: R.F. Smith <rsmith@xs4all.nl>
+// SPDX-License-Identifier: Unlicense
+// Created: 2025-09-08 20:48:54 +0200
+// Last modified: 2025-09-08T21:24:09+0200
+
+#include "arena.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#define ARENA_MAGIC 0x4152454e
+
+// For condition testing, e.g. a == b.
+#define test(exp) \
+  do \
+    if (exp) { \
+      printf("\033[0;32mPASSED:\033[0m " #exp "\n"); \
+    } else { \
+      printf("\033[1;31mFAILED:\033[0m " #exp "\n"); \
+    } \
+  while (0)
+
+
+int main(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
+  Arena std = arena_create(4096);
+  puts("After creating the arena.");
+  test(std.magic == ARENA_MAGIC);
+  test(std.current_offset == 0);
+  test(std.length == 4096);
+  char *fname = "arena.c";
+  ptrdiff_t size = 1024;
+  FILE *f = fopen(fname, "r");
+  if (f) {
+    fread(arena_new(&std, char, size), size, 1, f);
+    fclose(f);
+    puts("After adding to the arena.");
+    test(std.current_offset == size);
+    test(arena_remaining(&std) == (4096 - size));
+    test(memcmp(std.begin, "// file: arena.c", 16) == 0);
+    arena_empty(&std);
+    puts("After empying the arena.");
+    test(std.current_offset == 0);
+  }
+  return 0;
+}
