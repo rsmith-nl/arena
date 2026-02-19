c79484ee5a2bdee4ae8eff7ea1a2989b49ac3a2c
Roland Smith <rsmith@xs4all.nl>
2026-02-19 01:28:02 +0100

    Abort on failures.

--- arena.c
+++ arena.c
@@ -5,14 +5,14 @@
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2023-04-23T22:08:02+0200
-// Last modified: 2026-02-18T19:15:43+0100
+// Last modified: 2026-02-21T23:46:55+0100
 
 #include "arena.h"
-#include "logging.h"
 #include <assert.h>
 #include <stdio.h>      // for printf
 #include <stdint.h>     // for uintptr_t
 #include <stddef.h>     // for ptrdiff_t
+#include <stdlib.h>     // for abort
 #include <string.h>     // for memset
 #ifdef _WIN32
 #include <memoryapi.h>
@@ -37,7 +37,8 @@ Arena arena_create(ptrdiff_t length)
   arena.begin = mmap(0, length, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
 #endif
   if (arena.begin == MAP_FAILED) {
-    error("arena allocation of size %td failed\n", length);
+    fprintf(stderr, "arena allocation of size %td failed\n", length);
+    abort();
   }
   arena.current_offset = 0;
   arena.length = length;
@@ -58,8 +59,9 @@ void *arena_alloc(Arena *arena, ptrdiff_t size, ptrdiff_t count, ptrdiff_t align
   ptrdiff_t padding = -arena->current_offset & (align - 1);
   ptrdiff_t remaining = arena->length - arena->current_offset - padding;
   if (count > remaining/size) {
-    error("arena %p exhausted; %td items of %td bytes requested, %td available\n",
+    fprintf(stderr, "arena %p exhausted; %td items of %td bytes requested, %td available\n",
           (void *)arena, count, size, remaining/size);
+    abort();
   }
   void *rv = arena->begin + arena->current_offset + padding;
   arena->current_offset += padding + count * size;
@@ -79,7 +81,7 @@ void arena_destroy(Arena *arena)
   rv = munmap(arena->begin, arena->length);
 #endif
   if (rv == -1) {
-    error("destroying arena %p failed\n", (void *)arena);
+    fprintf(stderr, "destroying arena %p failed\n", (void *)arena);
   }
   Arena empty = {0};
   *arena = empty;
